b036229ca0faeb91364009a09d7a80e30a54f45c
skoosm990 <161984156+skoosm990@users.noreply.github.com>
2025-02-01 11:35:05 +0800

    assignment4

---
+++ assignment4/question5.js
@@ -0,0 +1,16 @@
+const disneyData = [
+	{
+		open: "120.54",
+		high: "122.56",
+		low: "120.54",
+		close: "121.09"
+	},
+	{
+		open: "123.15",
+		high: "127.76",
+		low: "123.02",
+		close: "124.92"
+	}
+];
+
+// Write your code below
---
+++ assignment4/question6.js
@@ -0,0 +1,8 @@
+// Modify the code below:
+
+// const amdPrices = [90.21, 89.15, 88.15, 87.12, 102.21, 100.12, 79.82]
+
+let data = ()
+for price in data:
+	if price < 3:
+    data.push(price)
